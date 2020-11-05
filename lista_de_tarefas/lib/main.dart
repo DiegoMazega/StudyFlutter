9f9147b31a6afd4c43d7c0d20f56b3fa4e97c381
DiegoMazega <diego.mazega@live.com>
2020-11-05 18:02:29 -0300

    Criando base do projeto Lista de Tarefas

---
+++ lista_de_tarefas/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/screens/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: new Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
